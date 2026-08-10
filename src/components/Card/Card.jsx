c7f4986c47f728ce6d02c9041f7037ec6d36e967
patoamaya <patriamayaford@gmail.com>
2026-08-10 16:00:24 -0300

    refactorizando 50%

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,12 +5,9 @@ import book from '../../assets/book.svg'
 const card = ({data}) => {
     const {titulo, autor, id, leido} = data
   return (
-    <div className= "cardContainer">
-        <div className={leido ? "book-img-leido" : "book-img-no-leido"}>
-        <img src= {book} alt=""/>
-        </div>
+    <div className= {leido ? "cardContainer" : "cardContainer-no-leido"}>
+            <h2>{autor}</h2>
         <ul>
-            <li><h3>{autor}</h3></li>
             <li><p>{titulo}</p></li>
             <li>{leido ? <h5>Leído</h5> : <h5>No leído</h5> }</li>
         </ul>
