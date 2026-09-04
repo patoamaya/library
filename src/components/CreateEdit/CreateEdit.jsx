9eb4c64364c5c88eca71f98cf049d9a1ffb04fd0
patoamaya <patriamayaford@gmail.com>
2026-09-04 15:45:26 -0300

    mui install

--- src/components/CreateEdit/CreateEdit.jsx
+++ src/components/CreateEdit/CreateEdit.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import { TextField } from '@mui/material'
 import './CreateEdit.css'
+
 const CreateEdit = ({datos}) => {
   const {id, data} = datos
+  const {titulo, autor, sinopsis, leido} = data
   return (
     <div>
       {
         id ? 
-        <div className="">
-        <h2>{id}</h2>
+        <div className="edit-container-all">
+        {/* <h2>{id}</h2> */}
+        <form action="post">
+          <TextField
+          required
+          id="outlined-required"
+          label="Titulo"
+          defaultValue="Hello World"
+          />
+          <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Hello World"
+          />
+          <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Hello World"
+          />
+          <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Hello World"
+          />
+
+        </form>
         </div>
         :
         <h2>No hay id</h2>
